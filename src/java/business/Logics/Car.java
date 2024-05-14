aa04d7a44a00a11efc4681b64d0dc7ffd9237f80
System Administrator <akilawijesinghe94@gmail.com>
2024-05-14 21:13:28 +1000

    Initial commit

---
+++ src/java/business/Logics/Car.java
@@ -0,0 +1,175 @@
+package business.Logics;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Car implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected Long id;
+    protected String make;
+    protected String model;
+    protected String referenceNumber;
+    protected String driveType;
+    protected String bodyColor;
+    protected String transmissionType;
+    protected String engineType;
+    protected String fuelType;
+    protected Integer numberOfDoors;
+    protected Integer numberOfSeats;
+    protected Integer price;
+    protected Integer carCount;
+
+    public Car() {
+    }
+
+    public Car(Long id, String make, String model, String referenceNumber, String driveType, String bodyColor, String transmissionType, String engineType, String fuelType, Integer numberOfDoors, Integer numberOfSeats, Integer price, Integer carCount) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.referenceNumber = referenceNumber;
+        this.driveType = driveType;
+        this.bodyColor = bodyColor;
+        this.transmissionType = transmissionType;
+        this.engineType = engineType;
+        this.fuelType = fuelType;
+        this.numberOfDoors = numberOfDoors;
+        this.numberOfSeats = numberOfSeats;
+        this.price = price;
+        this.carCount = carCount;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public String getReferenceNumber() {
+        return referenceNumber;
+    }
+
+    public void setReferenceNumber(String referenceNumber) {
+        this.referenceNumber = referenceNumber;
+    }
+
+    public Integer getCarCount() {
+        return carCount;
+    }
+
+    public void setCarCount(Integer carCount) {
+        this.carCount = carCount;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getDriveType() {
+        return driveType;
+    }
+
+    public void setDriveType(String driveType) {
+        this.driveType = driveType;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBodyColor() {
+        return bodyColor;
+    }
+
+    public void setBodyColor(String bodyColor) {
+        this.bodyColor = bodyColor;
+    }
+
+    public String getTransmissionType() {
+        return transmissionType;
+    }
+
+    public void setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+
+    public Integer getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(Integer numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public Integer getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(Integer numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Car)) {
+            return false;
+        }
+        Car other = (Car) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "assignment2.Car[ id=" + id + " ]";
+    }
+    
+}
