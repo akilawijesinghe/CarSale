eb99ef2a9e76d512ec69a2eab8e6adfab6165dc4
System Administrator <akilawijesinghe94@gmail.com>
2024-05-16 18:44:01 +1000

    Change Folder Structure

--- src/java/Controllers/CustomerController.java
+++ src/java/Controllers/CustomerController.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import EJB.CustomerEJB;
-import models.Customer;
+import Models.Customer;
 import jakarta.ejb.EJB;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
