584bb09c9dda498a1446c5cffe48e73544d7e836
kasuneranda1991 <kasun1209@gmail.com>
2024-05-19 15:45:26 +1000

    Complete Customer create | search | view details - without order related details

--- src/java/Controllers/CustomerController.java
+++ src/java/Controllers/CustomerController.java
@@ -15,13 +15,19 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Controller for managing customers.
+ *
  * @author : kasun eranda - 12216898
  */
 @Named(value = "customerController")
 @RequestScoped
+@Getter
+@Setter
+
 public class CustomerController implements Serializable {
 
     @EJB
@@ -29,9 +35,13 @@ public class CustomerController implements Serializable {
 
     private Customer customer = new Customer();
     private List<Customer> customerList = new ArrayList<Customer>();
+    private List<Customer> customerSearchList = new ArrayList<Customer>();
+
+    private String searchKey = new String();
 
     /**
      * Gets the current customer.
+     *
      * @return The current customer.
      */
     public Customer getCustomer() {
@@ -40,6 +50,7 @@ public class CustomerController implements Serializable {
 
     /**
      * Sets the current customer.
+     *
      * @param customer The customer to set.
      */
     public void setCustomer(Customer customer) {
@@ -48,6 +59,7 @@ public class CustomerController implements Serializable {
 
     /**
      * Creates a new customer.
+     *
      * @return null to stay on the same page.
      */
     public String doCreateCustomer() {
@@ -63,15 +75,47 @@ public class CustomerController implements Serializable {
         } catch (Exception e) {
             ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Customer hasn't been created", e.getMessage()));
         }
-        return null;
+        return "index.xhtml";
     }
-    
+
     /**
      * Get all the customers
      */
     public List<Customer> getCustomerList() {
-        
-	customerList = customerEJB.findCustomers();
-	return customerList;
+
+        customerList = customerEJB.findCustomers();
+        return customerList;
     }
+
+    /**
+     * Search customers
+     *
+     * @return Customer
+     */
+    public String getCustomerByName() {
+
+        customerSearchList = customerEJB.searchCustomer(searchKey);
+        return "searchList.xhtml";
+    }
+
+    /**
+     * Return the search result
+     *
+     * @return List<Customer>
+     */
+    public List<Customer> getCustomerSearchList() {
+        return customerSearchList;
+    }
+    
+    /**
+     * Display a customer details
+     * 
+     * @param id customer id
+     * @return String View
+     */
+    public String doViewDetails(int id){
+        this.customer = customerEJB.findACustomer(id);
+        return "detail.xhtml";
+    }
+
 }
--- src/java/EJB/CustomerEJB.java
+++ src/java/EJB/CustomerEJB.java
@@ -11,10 +11,12 @@ import jakarta.ejb.LocalBean;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 
 /**
  * EJB for managing customers.
+ *
  * @author : kasun eranda - 12216898
  */
 @Stateless
@@ -26,6 +28,7 @@ public class CustomerEJB {
 
     /**
      * Finds all customers.
+     *
      * @return A list of customers.
      */
     public List<Customer> findCustomers() {
@@ -35,11 +38,37 @@ public class CustomerEJB {
 
     /**
      * Creates a new customer.
+     *
      * @param customer The customer to create.
      * @return The created customer.
      */
     public Customer createCustomer(Customer customer) {
         em.persist(customer);
         return customer;
+    }
+
+    /**
+     * Search customers
+     *
+     * @param name name
+     * @return
+     */
+    public List<Customer> searchCustomer(String name) {
+        Query q = em.createNamedQuery("findWithParam");
+        q.setParameter("name", name);
+
+        return q.getResultList();
+    }
+    
+     /**
+     * Get a customer by id
+     *
+     * @param id customer id
+     * @return
+     */
+    public Customer findACustomer(int id) {
+        TypedQuery<Customer> query = em.createNamedQuery("findById", Customer.class);
+        query.setParameter("id", id);
+        return query.getSingleResult();
     }
 }
